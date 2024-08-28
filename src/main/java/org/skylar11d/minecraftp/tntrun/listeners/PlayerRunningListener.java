d406c03e65872094bea0cc6823f01a7c33d8f657
skylar11d <skylar11d@aol.com>
2024-08-28 18:04:19 +0300

    some tweaks

--- src/main/java/org/skylar11d/minecraftp/tntrun/listeners/PlayerRunningListener.java
+++ src/main/java/org/skylar11d/minecraftp/tntrun/listeners/PlayerRunningListener.java
@@ -1,15 +1,12 @@
 package org.skylar11d.minecraftp.tntrun.listeners;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.skylar11d.minecraftp.tntrun.Main;
 import org.skylar11d.minecraftp.tntrun.utilities.assets.events.PlayerRunEvent;
-import org.skylar11d.minecraftp.tntrun.utilities.config.ConfigType;
 import org.skylar11d.minecraftp.tntrun.utilities.manager.runner.Runner;
 import org.skylar11d.minecraftp.tntrun.utilities.manager.runner.RunnerManager;
 
-import java.util.concurrent.*;
 import java.util.function.Predicate;
 
 public class PlayerRunningListener implements Listener {
